b412469316d02ef1f99cb44dd2b17ce050f1cdbb
N8theGr8 <inatejh@gmail.com>
2018-08-08 18:22:47 -0500

    Initial commit

---
+++ src/PictureManipulator.java
@@ -0,0 +1,32 @@
+interface PictureManipulator {
+
+  void loadImage();
+
+  void highlightVertical();
+
+  void deleteVertical();
+
+  void insertVertical();
+
+  void highlightHorizontal();
+
+  void deleteHorizontal();
+
+  void insertHorizontal();
+
+  void highLightDiagonal();
+
+  void deleteDiagonal();
+
+  void insertDiagonal();
+
+  void applyMask();
+
+  void deleteMask();
+
+  void replaceMask();
+
+  void getImage();
+
+  void saveImage();
+}
